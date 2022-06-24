65c5584c577888c113cf42f94a6ef0b16e03398a
ArturChiodiSiqueira <arturchiodi@outlook.com>
2022-06-24 16:51:37 -0300

    att

---
+++ SistemaLogin/CadastroUsuarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaLogin
+{
+    static class CadastroUsuarios
+    {
+        private static Usuario[] usuarios =
+        {
+            new Usuario(){Nome = "Gabriel", Senha = "abc123"},
+            new Usuario(){Nome = "Danny", Senha = "123abc"},
+            new Usuario(){Nome = "Artur", Senha = "abcd"}
+        };
+
+        private static Usuario _userLogado = null;
+
+        public static Usuario UsuarioLogado { get { return _userLogado; } private set { _userLogado = value; } }
+
+        public static bool Login(string nome, string senha)
+        {
+            foreach (Usuario usuario in usuarios)
+            {
+
+            }
+            return false;
+        }
+    }
+}
